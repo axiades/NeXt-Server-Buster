1456772be28dd107088a6de978f7421daae0def3
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 12:50:32 +0100

    Update lets_encrypt.sh

--- script/lets_encrypt.sh
+++ script/lets_encrypt.sh
@@ -9,7 +9,7 @@ systemctl -q stop nginx.service
 mkdir -p /etc/nginx/ssl/
 
 install_packages "cron netcat-openbsd curl socat"
-cd /root/NeXt-Server-Buster/sources
+cd /root/Perfectrootserver/sources
 git clone https://github.com/Neilpang/acme.sh.git -q 
 cd ./acme.sh
 sleep 1
@@ -23,7 +23,7 @@ create_nginx_cert() {
 
 systemctl -q stop nginx.service
 
-cd /root/NeXt-Server-Buster/sources/acme.sh/
+cd /root/Perfectrootserver/sources/acme.sh/
 bash acme.sh --issue --standalone --debug 2 --log -d ${MYDOMAIN} -d www.${MYDOMAIN} --keylength ec-384 
 
 ln -s /root/.acme.sh/${MYDOMAIN}_ecc/fullchain.cer /etc/nginx/ssl/${MYDOMAIN}-ecc.cer 
@@ -35,8 +35,8 @@ HPKP2=$(openssl rand -base64 32)
 sed_replace_word "HPKP1" "${HPKP1}" "/etc/nginx/security.conf"
 sed_replace_word "HPKP2" "${HPKP2}" "/etc/nginx/security.conf"
 
-sed -i 's/HPKP1="1"/HPKP1="'${HPKP1}'"/' /root/NeXt-Server-Buster/configs/userconfig.cfg
-sed -i 's/HPKP2="2"/HPKP2="'${HPKP2}'"/' /root/NeXt-Server-Buster/configs/userconfig.cfg
+sed -i 's/HPKP1="1"/HPKP1="'${HPKP1}'"/' /root/Perfectrootserver/configs/userconfig.cfg
+sed -i 's/HPKP2="2"/HPKP2="'${HPKP2}'"/' /root/Perfectrootserver/configs/userconfig.cfg
 
 ### change path --> see system.sh
 echo "0 0 1 */3 *"   root    bash /etc/cron.d/le_cert_alert >> /etc/crontab
@@ -44,7 +44,7 @@ echo "0 0 1 */3 *"   root    bash /etc/cron.d/le_cert_alert >> /etc/crontab
 
 update_nginx_cert() {
 
-source /root/NeXt-Server-Buster/configs/sources.cfg
+source /root/Perfectrootserver/configs/sources.cfg
 
 systemctl -q stop nginx.service
 
@@ -54,12 +54,12 @@ rm /etc/nginx/ssl/${MYDOMAIN}-ecc.cer
 rm /etc/nginx/ssl/${MYDOMAIN}-ecc.key
 
 #delete old keys
-sed -i 's/HPKP1="'${HPKP1}'"/HPKP1="1"/' /root/NeXt-Server-Buster/configs/userconfig.cfg
-sed -i 's/HPKP2="'${HPKP2}'"/HPKP2="2"/' /root/NeXt-Server-Buster/configs/userconfig.cfg
+sed -i 's/HPKP1="'${HPKP1}'"/HPKP1="1"/' /root/Perfectrootserver/configs/userconfig.cfg
+sed -i 's/HPKP2="'${HPKP2}'"/HPKP2="2"/' /root/Perfectrootserver/configs/userconfig.cfg
 sed -i "s/"${HPKP1}"/HPKP1/g" /etc/nginx/security.conf
 sed -i "s/"${HPKP2}"/HPKP2/g" /etc/nginx/security.conf
 
-cd /root/NeXt-Server-Buster/sources/acme.sh/
+cd /root/Perfectrootserver/sources/acme.sh/
 bash acme.sh --issue --standalone --debug 2 --log -d ${MYDOMAIN} -d www.${MYDOMAIN} --keylength ec-384 
 
 ln -s /root/.acme.sh/${MYDOMAIN}_ecc/fullchain.cer /etc/nginx/ssl/${MYDOMAIN}-ecc.cer 
@@ -75,8 +75,8 @@ HPKP2=$(echo "$HPKP2" | sed 's/\//\\\//g')
 sed_replace_word "HPKP1" "${HPKP1}" "/etc/nginx/security.conf"
 sed_replace_word "HPKP2" "${HPKP2}" "/etc/nginx/security.conf"
 
-sed -i 's/HPKP1="1"/HPKP1="'${HPKP1}'"/' /root/NeXt-Server-Buster/configs/userconfig.cfg
-sed -i 's/HPKP2="2"/HPKP2="'${HPKP2}'"/' /root/NeXt-Server-Buster/configs/userconfig.cfg
+sed -i 's/HPKP1="1"/HPKP1="'${HPKP1}'"/' /root/Perfectrootserver/configs/userconfig.cfg
+sed -i 's/HPKP2="2"/HPKP2="'${HPKP2}'"/' /root/Perfectrootserver/configs/userconfig.cfg
 
 systemctl -q start nginx.service
 }
@@ -85,7 +85,7 @@ update_mailserver_cert() {
 
 
 
-source /root/NeXt-Server-Buster/configs/sources.cfg
+source /root/Perfectrootserver/configs/sources.cfg
 
 systemctl -q stop nginx.service
 
