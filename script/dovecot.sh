5796f64eca0bfaba4c496f5fde058faae407ac68
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 12:47:31 +0100

    Update dovecot.sh

--- script/dovecot.sh
+++ script/dovecot.sh
@@ -12,12 +12,12 @@ mkdir -p /etc/dovecot
 cd /etc/dovecot
 
 openssl dhparam -out /etc/dovecot/dh.pem 2048 >/dev/null 2>&1
-cp /root/NeXt-Server-Buster/configs/dovecot/dovecot.conf /etc/dovecot/dovecot.conf
+cp /root/Perfectrootserver/configs/dovecot/dovecot.conf /etc/dovecot/dovecot.conf
 sed_replace_word "domain.tld" "${MYDOMAIN}" "/etc/dovecot/dovecot.conf"
 
-cp /root/NeXt-Server-Buster/configs/dovecot/dovecot-sql.conf /etc/dovecot/dovecot-sql.conf
+cp /root/Perfectrootserver/configs/dovecot/dovecot-sql.conf /etc/dovecot/dovecot-sql.conf
 sed_replace_word "placeholder" "${MAILSERVER_DB_PASS}" "/etc/dovecot/dovecot-sql.conf"
 chmod 440 /etc/dovecot/dovecot-sql.conf
 
-cp /root/NeXt-Server-Buster/configs/dovecot/sieve/* /var/vmail/sieve/global/
+cp /root/Perfectrootserver/configs/dovecot/sieve/* /var/vmail/sieve/global/
 }
