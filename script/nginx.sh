06beac844ff9d5a0709ed28e4296fcb72eb2c256
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 12:53:26 +0100

    Update nginx.sh

--- script/nginx.sh
+++ script/nginx.sh
@@ -7,7 +7,7 @@ trap error_exit ERR
 
 install_packages "psmisc libpcre3 libpcre3-dev libgeoip-dev zlib1g-dev"
 
-cd /root/NeXt-Server-Buster/sources
+cd /root/Perfectrootserver/sources
 wget_tar "https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
 tar_file "nginx-${NGINX_VERSION}.tar.gz"
 cd nginx-${NGINX_VERSION} 
@@ -56,16 +56,16 @@ NGINX_MODULES="--without-http_browser_module \
 --with-http_mp4_module \
 --with-http_gunzip_module \
 --with-openssl-opt=enable-tls1_3 \
---with-openssl=/root/NeXt-Server-Buster/sources/openssl-${OPENSSL_VERSION} \
---add-module=/root/NeXt-Server-Buster/sources/naxsi/naxsi_src \
---add-module=/root/NeXt-Server-Buster/sources/incubator-pagespeed-ngx-${NPS_VERSION} \
---add-module=/root/NeXt-Server-Buster/sources/headers-more-nginx-module-${NGINX_HEADER_MOD_VERSION}"
+--with-openssl=/root/Perfectrootserver/sources/openssl-${OPENSSL_VERSION} \
+--add-module=/root/Perfectrootserver/sources/naxsi/naxsi_src \
+--add-module=/root/Perfectrootserver/sources/incubator-pagespeed-ngx-${NPS_VERSION} \
+--add-module=/root/Perfectrootserver/sources/headers-more-nginx-module-${NGINX_HEADER_MOD_VERSION}"
 
 ./configure $NGINX_OPTIONS $NGINX_MODULES --with-cc-opt='-O2 -g -pipe -Wall -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong -m64 -mtune=generic' 
 make -j $(nproc) 
 make install 
 
-rm -R /root/NeXt-Server-Buster/sources/nginx-${NGINX_VERSION}
+rm -R /root/Perfectrootserver/sources/nginx-${NGINX_VERSION}
 
 mkdir -p /var/lib/nginx/{body,proxy,fastcgi,uwsgi,scgi}
 mkdir -p /etc/nginx/{sites,ssl,sites-available,sites-enabled,htpasswd}
@@ -75,7 +75,7 @@ mkdir -p /var/www/${MYDOMAIN}/public
 mkdir -p /var/cache/nginx
 mkdir -p /var/log/nginx/
 
-cd /root/NeXt-Server-Buster
+cd /root/Perfectrootserver
 
 wget_tar "-O /etc/init.d/nginx -c4 --no-check-certificate https://raw.githubusercontent.com/Fleshgrinder/nginx-sysvinit-script/master/init"
 chmod 0755 /etc/init.d/nginx 
@@ -84,31 +84,16 @@ chown root:root /etc/init.d/nginx
 update-rc.d nginx defaults 
 
 rm -rf /etc/nginx/nginx.conf
-cp /root/NeXt-Server-Buster/configs/nginx/confs/* /etc/nginx/
+cp /root/Perfectrootserver/configs/nginx/confs/* /etc/nginx/
 
 rm -rf /etc/nginx/sites-available/${MYDOMAIN}.conf
-cp /root/NeXt-Server-Buster/configs/nginx/vhost /etc/nginx/sites-available/${MYDOMAIN}.conf
+cp /root/Perfectrootserver/configs/nginx/vhost /etc/nginx/sites-available/${MYDOMAIN}.conf
 sed_replace_word "MYDOMAIN" "${MYDOMAIN}" "/etc/nginx/sites-available/${MYDOMAIN}.conf"
 sed_replace_word "MYDOMAIN" "${MYDOMAIN}" "/etc/nginx/_pagespeed.conf"
-
-if [[ ${IPV4_ONLY} = "1" ]]; then
-  sed_replace_word "IPADR" "${IPADR}" "/etc/nginx/sites-available/${MYDOMAIN}.conf"
-fi
-
-if [[ ${IPV6_ONLY} = "1" ]]; then
-### doesn't work at all, bc hes adding ipv4 too	
-  sed_replace_word "IPADR" ":" "/etc/nginx/sites-available/${MYDOMAIN}.conf"
-  sed_replace_word "IP6ADR" "${IP6ADR}" "/etc/nginx/sites-available/${MYDOMAIN}.conf"
-fi
-
-if [[ ${IP_DUAL} == '1' ]]; then
-  sed_replace_word "IPADR" "${IPADR}" "/etc/nginx/sites-available/${MYDOMAIN}.conf"
-  sed_replace_word "IP6ADR" "${IP6ADR}" "/etc/nginx/sites-available/${MYDOMAIN}.conf"
-fi
 
 chown -R www-data:www-data /var/www/${MYDOMAIN}/public
 ln -s /etc/nginx/sites-available/${MYDOMAIN}.conf /etc/nginx/sites-enabled/${MYDOMAIN}.conf
 
-cp /root/NeXt-Server-Buster/includes/NeXt-logo.jpg /var/www/${MYDOMAIN}/public/NeXt-logo.jpg
-cp /root/NeXt-Server-Buster/configs/nginx/index.html /var/www/${MYDOMAIN}/public/index.html
+cp /root/Perfectrootserver/includes/NeXt-logo.jpg /var/www/${MYDOMAIN}/public/NeXt-logo.jpg
+cp /root/Perfectrootserver/configs/nginx/index.html /var/www/${MYDOMAIN}/public/index.html
 }
