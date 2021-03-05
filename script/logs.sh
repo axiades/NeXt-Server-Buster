6cb273cdc2f6d0ec443ace37bd91ab2116a06ee9
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 12:50:52 +0100

    Update logs.sh

--- script/logs.sh
+++ script/logs.sh
@@ -2,9 +2,9 @@
 #Please check the license provided with the script!
 
 set_logs() {
-main_log="/root/NeXt-Server-Buster/logs/main.log"
-err_log="/root/NeXt-Server-Buster/logs/error.log"
-failed_checks_log="/root/NeXt-Server-Buster/logs/failed_checks.log"
-make_log="/root/NeXt-Server-Buster/logs/make.log"
-make_err_log="/root/NeXt-Server-Buster/logs/make_error.log"
+main_log="/root/Perfectrootserver/logs/main.log"
+err_log="/root/Perfectrootserver/logs/error.log"
+failed_checks_log="/root/Perfectrootserver/logs/failed_checks.log"
+make_log="/root/Perfectrootserver/logs/make.log"
+make_err_log="/root/Perfectrootserver/logs/make_error.log"
 }
