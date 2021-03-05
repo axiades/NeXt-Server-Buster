8dcb12b267c27e5b83ce7db731ca1df0e74d6580
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 13:00:47 +0100

    Update addons_menu.sh

--- menus/addons_menu.sh
+++ menus/addons_menu.sh
@@ -3,28 +3,23 @@
 
 menu_options_addons() {
 
-source /root/NeXt-Server-Buster/configs/sources.cfg
+source /root/Perfectrootserver/configs/sources.cfg
 set_logs
 
 HEIGHT=40
 WIDTH=80
 CHOICE_HEIGHT=12
-BACKTITLE="NeXt Server"
-TITLE="NeXt Server"
+BACKTITLE="Perfectrootserver"
+TITLE="Perfectrootserver"
 MENU="Choose one of the following options:"
 
 OPTIONS=(1 "Install TS3 Server"
          2 "Deinstall TS3 Server"
          3 "Install Composer"
-         4 "Install Nextcloud"
-         5 "Deinstall Nextcloud"
-         6 "Install phpmyadmin"
-         7 "Deinstall phpmyadmin"
-         8 "Install Munin"
-         9 "Install Wordpress"
-         10 "Deinstall Wordpress"
-         11 "Back"
-         12 "Exit")
+         4 "Install phpmyadmin"
+         5 "Deinstall phpmyadmin"
+         6 "Back"
+         7 "Exit")
 
 CHOICE=$(dialog --clear \
                 --nocancel \
@@ -40,16 +35,16 @@ clear
 case $CHOICE in
 
 1)
-if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' ]]; then
+if [[ ${IS_INSTALLED} == '1' ]] || [[ ${IS_INSTALLED_MAILSERVER} == '1' ]]; then
     if [[ ${TS3_IS_INSTALLED} == '1' ]]; then
         echo "Teamspeak 3 is already installed!"
     else
         dialog_info "Installing Teamspeak 3"
         install_teamspeak3
-        dialog_msg "Finished installing Teamspeak 3! Credentials: /root/NeXt-Server-Buster/teamspeak3_login_data.txt"
+        dialog_msg "Finished installing Teamspeak 3! Credentials: /root/Perfectrootserver/teamspeak3_login_data.txt"
     fi
 else
-    echo "You have to install the NeXt Server to run this Addon!"
+    echo "You have to install the Server to run this Addon!"
 fi
 continue_or_exit
 menu_options_addons
@@ -71,7 +66,7 @@ menu_options_addons
 ;;
 
 3)
-if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' ]]; then
+if [[ ${IS_INSTALLED} == '1' ]] || [[ ${IS_INSTALLED_MAILSERVER} == '1' ]]; then
     if [[ ${COMPOSER_IS_INSTALLED} == '1' ]]; then
         echo "Composer is already installed!"
     else
@@ -80,42 +75,14 @@ if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' 
         dialog_msg "Finished installing Composer"
     fi
 else
-    echo "You have to install the NeXt Server with the Webserver component to run this Addon!"
+    echo "You have to install the Server with the Webserver component to run this Addon!"
 fi
 continue_or_exit
 menu_options_addons
 ;;
 
 4)
-if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' ]]; then
-    if [[ ${NEXTCLOUD_IS_INSTALLED} == '1' ]]; then
-        echo "Nextcloud is already installed!"
-    else
-        menu_options_nextcloud
-        install_nextcloud
-        dialog --title "Your Nextcloud logininformations" --tab-correct --exit-label "ok" --textbox /root/NeXt-Server-Buster/nextcloud_login_data.txt 50 200
-    fi
-else
-    echo "You have to install the NeXt Server with the Webserver component to run this Addon!"
-fi
-continue_or_exit
-menu_options_addons
-;;
-
-5)
-if [[ ${NEXTCLOUD_IS_INSTALLED} == '0' ]]; then
-    echo "Nextcloud is already deinstalled!"
-else
-    dialog_info "Deinstalling Nextcloud"
-    deinstall_nextcloud
-    dialog_msg "Finished Deinstalling Nextcloud"
-fi
-continue_or_exit
-menu_options_addons
-;;
-
-6)
-if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' ]]; then
+if [[ ${IS_INSTALLED} == '1' ]] || [[ ${IS_INSTALLED_MAILSERVER} == '1' ]]; then
     if [[ ${PMA_IS_INSTALLED} == '1' ]]; then
         echo "Phpmyadmin is already installed!"
     else
@@ -127,13 +94,13 @@ if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' 
         dialog_msg "Finished installing PHPmyadmin"
     fi
 else
-    echo "You have to install the NeXt Server with the Webserver component to run this Addon!"
+    echo "You have to install the Server with the Webserver component to run this Addon!"
 fi
 continue_or_exit
 menu_options_addons
 ;;
 
-7)
+5)
 if [[ ${PMA_IS_INSTALLED} == '0' ]]; then
     echo "Phpmyadmin is already deinstalled!"
 else
@@ -145,60 +112,11 @@ continue_or_exit
 menu_options_addons
 ;;
 
-8)
-if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' ]]; then
-    if [[ ${MUNIN_IS_INSTALLED} == '1' ]]; then
-        echo "Munin is already installed!"
-    else
-        dialog_info "Installing Munin"
-        menu_options_munin
-        install_munin
-        dialog_msg "Finished installing Munin"
-    fi
-else
-    echo "You have to install the NeXt Server with the Webserver component to run this Addon!"
-fi
-continue_or_exit
-menu_options_addons
+6)
+bash /root/Perfectrootserver/prs.sh
 ;;
 
-9)
-if [[ ${NXT_IS_INSTALLED} == '1' ]] || [[ ${NXT_IS_INSTALLED_MAILSERVER} == '1' ]]; then
-    if [[ ${WORDPRESS_IS_INSTALLED} == '1' ]]; then
-        echo "Wordpress is already installed!"
-    else
-        menu_options_wordpress
-        install_wordpress
-        if [ "${WORDPRESS_PATH_NAME}" != "root" ]; then
-           dialog_msg "Visit ${MYDOMAIN}/${WORDPRESS_PATH_NAME} to finish the installation"
-        else
-           dialog_msg "Visit ${MYDOMAIN}/ to finish the installation"
-        fi
-    fi
-else
-    echo "You have to install the NeXt Server with the Webserver component to run this Addon!"
-fi
-continue_or_exit
-menu_options_addons
-;;
-
-10)
-if [[ ${WORDPRESS_IS_INSTALLED} == '0' ]]; then
-    echo "Wordpress is already deinstalled!"
-else
-    dialog_info "Deinstalling Wordpress"
-    deinstall_wordpress
-    dialog_msg "Finished Deinstalling Wordpress"
-fi
-continue_or_exit
-menu_options_addons
-;;
-
-11)
-bash /root/NeXt-Server-Buster/nxt.sh
-;;
-
-12)
+7)
 echo "Exit"
 exit 1
 ;;
