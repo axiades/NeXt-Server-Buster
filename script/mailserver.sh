7a061e40429709bdafea8b3192e1c0c8124a070b
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 12:51:25 +0100

    Update mailserver.sh

--- script/mailserver.sh
+++ script/mailserver.sh
@@ -6,7 +6,7 @@ install_mailserver() {
 trap error_exit ERR
 
 systemctl -q stop nginx.service
-cd /root/NeXt-Server-Buster/sources/acme.sh/
+cd /root/Perfectrootserver/sources/acme.sh/
 bash acme.sh --issue --debug 2 --standalone -d mail.${MYDOMAIN} -d imap.${MYDOMAIN} -d smtp.${MYDOMAIN} --keylength 4096 
 ln -s /root/.acme.sh/mail.${MYDOMAIN}/fullchain.cer /etc/nginx/ssl/mail.${MYDOMAIN}.cer
 ln -s /root/.acme.sh/mail.${MYDOMAIN}/mail.${MYDOMAIN}.key /etc/nginx/ssl/mail.${MYDOMAIN}.key
@@ -14,15 +14,15 @@ systemctl -q start nginx.service
 
 MAILSERVER_DB_PASS=$(password)
 
-echo "#------------------------------------------------------------------------------#" >> /root/NeXt-Server-Buster/login_information.txt
-echo "MAILSERVER_DB_PASS: $MAILSERVER_DB_PASS" >> /root/NeXt-Server-Buster/login_information.txt
-echo "#------------------------------------------------------------------------------#" >> /root/NeXt-Server-Buster/login_information.txt
-echo "" >> /root/NeXt-Server-Buster/login_information.txt
+echo "#------------------------------------------------------------------------------#" >> /root/Perfectrootserver/login_information.txt
+echo "MAILSERVER_DB_PASS: $MAILSERVER_DB_PASS" >> /root/Perfectrootserver/login_information.txt
+echo "#------------------------------------------------------------------------------#" >> /root/Perfectrootserver/login_information.txt
+echo "" >> /root/Perfectrootserver/login_information.txt
 
-sed_replace_word "placeholder" "${MAILSERVER_DB_PASS}" "/root/NeXt-Server-Buster/configs/mailserver/database.sql"
-mysql -u root -p${MYSQL_ROOT_PASS} mysql < /root/NeXt-Server-Buster/configs/mailserver/database.sql
+sed_replace_word "placeholder" "${MAILSERVER_DB_PASS}" "/root/Perfectrootserver/configs/mailserver/database.sql"
+mysql -u root -p${MYSQL_ROOT_PASS} mysql < /root/Perfectrootserver/configs/mailserver/database.sql
 
-mysql -u root -p${MYSQL_ROOT_PASS} mysql < /root/NeXt-Server-Buster/configs/mailserver/tlspolicies.sql
+mysql -u root -p${MYSQL_ROOT_PASS} mysql < /root/Perfectrootserver/configs/mailserver/tlspolicies.sql
 
 adduser --gecos "" --disabled-login --disabled-password --home /var/vmail vmail 
 
