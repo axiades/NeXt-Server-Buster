46a42356b58f04c9ad9ab02747e0a0fcfb25bbd4
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 12:49:36 +0100

    Update functions.sh

--- script/functions.sh
+++ script/functions.sh
@@ -141,13 +141,12 @@ error_exit() {
   USED_OS=$(lsb_release -ic)
   echo "Your used OS is: $USED_OS"
   echo ""
-  echo "If you don't know how to resolve this Issue, please visit https://github.com/shoujii/NeXt-Server-Buster/issues/new to add the Issue on Github!"
   exit
 }
 
 show_login_information() {
   dialog_msg "Please save the shown login information on next page"
-  cat /root/NeXt-Server-Buster/login_information.txt
+  cat /root/Perfectrootserver/login_information.txt
 }
 
 continue_to_menu() {
@@ -156,7 +155,7 @@ continue_to_menu() {
   echo "Exit"
   exit
   fi
-  bash /root/NeXt-Server-Buster/nxt.sh
+  bash /root/Perfectrootserver/nxt.sh
 }
 
 continue_or_exit() {
@@ -183,8 +182,8 @@ progress_gauge() {
 
 kernel_check_failed() {
 LOCAL_KERNEL_VERSION=$(uname -a | awk '/Linux/ {print $(NF-7)}') 
-BACKTITLE="NeXt Server Installation"
-TITLE="NeXt Server Installation"
+BACKTITLE="Perfectrootserver Installation"
+TITLE="Perfectrootserver Installation"
 HEIGHT=15
 WIDTH=70
 
