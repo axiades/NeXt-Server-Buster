0f1a4671b0ecd7379d0c79b676b3dcd5892ba1e8
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-05 12:53:45 +0100

    Update nginx_addons.sh

--- script/nginx_addons.sh
+++ script/nginx_addons.sh
@@ -7,7 +7,7 @@ trap error_exit ERR
 
 install_packages "autoconf automake libtool git unzip zlib1g-dev libpcre3 libpcre3-dev uuid-dev"
 
-cd /root/NeXt-Server-Buster/sources
+cd /root/Perfectrootserver/sources
 wget_tar "https://codeload.github.com/pagespeed/ngx_pagespeed/zip/v${NPS_VERSION}"
 unzip_file "v${NPS_VERSION}"
 cd incubator-pagespeed-ngx-${NPS_VERSION}/ 
@@ -15,10 +15,10 @@ cd incubator-pagespeed-ngx-${NPS_VERSION}/
 wget_tar "https://dl.google.com/dl/page-speed/psol/${PSOL_VERSION}-x64.tar.gz"
 tar_file "${PSOL_VERSION}-x64.tar.gz"
 
-cd /root/NeXt-Server-Buster/sources
+cd /root/Perfectrootserver/sources
 wget_tar "https://codeload.github.com/openresty/headers-more-nginx-module/zip/v${NGINX_HEADER_MOD_VERSION}"
 unzip_file "v${NGINX_HEADER_MOD_VERSION}"
 
-cd /root/NeXt-Server-Buster/sources
+cd /root/Perfectrootserver/sources
 git clone https://github.com/nbs-system/naxsi.git -q 
 }
