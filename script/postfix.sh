87a6da90a6ceeedeae94c685a40bcb2f4ef5d1b6
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-06 14:11:57 +0100

    Update postfix.sh

--- script/postfix.sh
+++ script/postfix.sh
@@ -13,16 +13,16 @@ cd /etc/postfix
 rm -r sasl
 rm master.cf main.cf.proto master.cf.proto
 
-cp /root/NeXt-Server-Buster/configs/postfix/main.cf /etc/postfix/main.cf
+cp /root/Perfectrootserver/configs/postfix/main.cf /etc/postfix/main.cf
 sed_replace_word "domain.tld" "${MYDOMAIN}" "/etc/postfix/main.cf"
 IPADR=$(ip route get 1.1.1.1 | awk '/1.1.1.1/ {print $(NF-2)}')
 sed_replace_word "changeme" "${IPADR}" "/etc/postfix/main.cf"
 
-cp /root/NeXt-Server-Buster/configs/postfix/master.cf /etc/postfix/master.cf
-cp /root/NeXt-Server-Buster/configs/postfix/submission_header_cleanup /etc/postfix/submission_header_cleanup
+cp /root/Perfectrootserver/configs/postfix/master.cf /etc/postfix/master.cf
+cp /root/Perfectrootserver/configs/postfix/submission_header_cleanup /etc/postfix/submission_header_cleanup
 
 mkdir /etc/postfix/sql
-cp -R /root/NeXt-Server-Buster/configs/postfix/sql/* /etc/postfix/sql/
+cp -R /root/Perfectrootserver/configs/postfix/sql/* /etc/postfix/sql/
 sed_replace_word "placeholder" "${MAILSERVER_DB_PASS}" "/etc/postfix/sql/accounts.cf"
 sed_replace_word "placeholder" "${MAILSERVER_DB_PASS}" "/etc/postfix/sql/aliases.cf"
 sed_replace_word "placeholder" "${MAILSERVER_DB_PASS}" "/etc/postfix/sql/domains.cf"
