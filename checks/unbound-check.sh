755efde24f59a0a5c3318377d749fb75f2cebb10
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-06 14:55:24 +0100

    Update unbound-check.sh

--- checks/unbound-check.sh
+++ checks/unbound-check.sh
@@ -31,7 +31,7 @@ echo "Unbound:"
 echo "${ok} ${passed_unbound_checks} checks passed!"
 
 if [[ "${failed_unbound_checks}" != "0" ]]; then
-  echo "${error} ${failed_unbound_checks} check/s failed! Please check /root/NeXt-Server-Buster/logs/failed_checks.log or consider a new installation!"
+  echo "${error} ${failed_unbound_checks} check/s failed! Please check failed_checks.log or consider a new installation!"
 fi
 
 check_service "unbound"
