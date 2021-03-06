a51ccad2b67e49b0fcec40cfff1cea1478bc41da
Phillip Lurgenstein <19848667+axiades@users.noreply.github.com>
2021-03-06 14:51:42 +0100

    Update fail2ban-check.sh

--- checks/fail2ban-check.sh
+++ checks/fail2ban-check.sh
@@ -31,7 +31,7 @@ echo "Fail2ban:"
 echo "${ok} ${passed_fail2ban_checks} checks passed!"
 
 if [[ "${failed_fail2ban_checks}" != "0" ]]; then
-  echo "${error} ${failed_fail2ban_checks} check/s failed! Please check /root/NeXt-Server-Buster/logs/failed_checks.log or consider a new installation!"
+  echo "${error} ${failed_fail2ban_checks} check/s failed! Please check failed_checks.log or consider a new installation!"
 fi
 
 check_service "fail2ban"
